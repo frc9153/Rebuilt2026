a3f933e72edb3f88e5c7015e93c1d4c1d640793c
catboychoreo <135279465+catboychoreo@users.noreply.github.com>
2026-02-25 13:06:24 -0600

    Definitely New Drive/Swerve
    This is not from last year. Why would it be from last year. Are you crazy?

--- constants.py
+++ constants.py
@@ -5,6 +5,62 @@
 #
 
 # Ok so basically this stuff just gives our motors a number to reference. It'll change. Oh gosh
+import math
+
+from wpimath import units
+from wpimath.geometry import Translation2d
+from wpimath.kinematics import SwerveDrive4Kinematics
+from wpimath.trajectory import TrapezoidProfileRadians
+
+from rev import SparkMax, SparkBaseConfig
+
+
+class NeoMotorConstants:
+    kFreeSpeedRpm = 5676
+
+
+class DriveConstants:
+    # Driving Parameters - Note that these are not the maximum capable speeds of
+    # the robot, rather the allowed maximum speeds
+    kMaxSpeedMetersPerSecond = 0.5 # 4.8
+    kMaxAngularSpeed = math.tau  # radians per second
+
+    kDirectionSlewRate = 1.2  # radians per second
+    kMagnitudeSlewRate = 1.8  # percent per second (1 = 100%)
+    kRotationalSlewRate = 2.0  # percent per second (1 = 100%)
+
+    # Chassis configuration
+    kTrackWidth = units.inchesToMeters(26.5)
+    # Distance between centers of right and left wheels on robot
+    kWheelBase = units.inchesToMeters(26.5)
+
+    # Distance between front and back wheels on robot
+    kModulePositions = [
+        Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+        Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+    ]
+    kDriveKinematics = SwerveDrive4Kinematics(*kModulePositions)
+
+    # Angular offsets of the modules relative to the chassis in radians
+    kFrontLeftChassisAngularOffset = -math.pi / 2
+    kFrontRightChassisAngularOffset = 0
+    kBackLeftChassisAngularOffset = math.pi
+    kBackRightChassisAngularOffset = math.pi / 2
+
+    # SPARK MAX CAN IDs
+    kFrontLeftDrivingCanId = 7
+    kRearLeftDrivingCanId = 5
+    kFrontRightDrivingCanId = 1
+    kRearRightDrivingCanId = 3
+
+    kFrontLeftTurningCanId = 8
+    kRearLeftTurningCanId = 6
+    kFrontRightTurningCanId = 2
+    kRearRightTurningCanId = 4
+
+    kGyroReversed = False
 
 class turretMotorConstants: 
     TURRET_VERTICAL_MOTOR = 1
